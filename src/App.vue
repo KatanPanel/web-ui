d1cd85fbb299aa01fe5b6a822b612229e617d2e7
DevNatan <itroken99@gmail.com>
2020-09-15 16:25:09 -0300

    Dedicated environment variables file.

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,19 @@
 		<router-view />
 	</div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { MetaInfo } from "vue-meta";
+
+@Component<App>({
+	metaInfo(): MetaInfo {
+		return {
+			title: "Homepage",
+			titleTemplate: `%s • ${process.env.VUE_APP_NAME} v${process.env.VUE_APP_VERSION}`
+		};
+	}
+})
+export default class App extends Vue {
+}
+</script>
 <style lang="scss" src="./assets/main.scss"></style>
