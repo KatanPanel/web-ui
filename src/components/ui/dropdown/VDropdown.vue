3d389fc378f59f1383af32f317d0d4e4dc2f9259
DevNatan <itroken99@gmail.com>
2021-02-17 13:18:57 -0300

    refactor: move dropdown native functions to component definition

--- src/components/ui/dropdown/VDropdown.vue
+++ src/components/ui/dropdown/VDropdown.vue
@@ -2,7 +2,8 @@
 	<component
 		:is="tag"
 		@click="toggle"
-		:class="{ 'v--dropdown-active': isOpen, [ELEMENT_CLASS]: true }"
+		:class="{ 'v--dropdown-active': isOpen }"
+		class="v--dropdown"
 	>
 		<slot />
 		<transition name="v--dropdown-transition" mode="out-in">
@@ -15,23 +16,10 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
-import { isChildNode } from "@/utils/dom";
+import { isChildNode } from "@/common/utils/dom";
 
-@Component
-export default class VDropdown extends Vue {
-	private readonly ELEMENT_CLASS = "v--dropdown";
-
-	@Prop({ type: String, required: true }) private readonly tag!: string;
-
-	isOpen = false;
-	closeListener!: any;
-
-	toggle(e: MouseEvent): void {
-		if (e.target && isChildNode(this.$el, e.target as Node))
-			this.isOpen = !this.isOpen;
-	}
-
-	mounted() {
+@Component<VDropdown>({
+	mounted(): void {
 		this.closeListener = document.addEventListener(
 			"click",
 			(e: MouseEvent) => {
@@ -39,10 +27,22 @@ export default class VDropdown extends Vue {
 					this.isOpen = false;
 			}
 		);
-	}
-
-	destroyed() {
+	},
+	destroyed(): void {
 		document.removeEventListener("click", this.closeListener);
+	},
+})
+export default class VDropdown extends Vue {
+	@Prop({ type: String, required: true, default: "div" })
+	private readonly tag!: string;
+	@Prop({ type: Boolean, default: true }) private readonly closed!: boolean;
+
+	private isOpen = !this.closed;
+	closeListener!: any;
+
+	private toggle(e: MouseEvent): void {
+		if (e.target && isChildNode(this.$el, e.target as Node))
+			this.isOpen = !this.isOpen;
 	}
 }
 </script>
