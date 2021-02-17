1ea532ba6e1bfab49dd3082e47ab902d383bc2dd
DevNatan <itroken99@gmail.com>
2021-02-17 13:49:11 -0300

    refactor: Vuex initialization have been moved to main

--- src/App.vue
+++ src/App.vue
@@ -5,49 +5,8 @@
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { MetaInfo } from "vue-meta";
-import { FALLBACK_LANGUAGE, setLanguage } from "@/i18n";
-import { DARK_THEME } from "@/store/state";
-import { dispatch } from "@/utils/vuex";
-import { UPDATE_THEME } from "@/store/actions";
-import { ROOT_MODULE, THEME_CACHE_KEY } from "@/store";
-import { AxiosError, AxiosResponse } from "axios";
 
-@Component<App>({
-	metaInfo(): MetaInfo {
-		return {
-			titleTemplate: `${process.env.VUE_APP_NAME}`,
-		};
-	},
-	beforeCreate(): void {
-		// setLanguage needs to be here to load Vuex.
-		setLanguage(FALLBACK_LANGUAGE);
-
-		if (!this.$storage.has(THEME_CACHE_KEY)) {
-			if (
-				window.matchMedia &&
-				window.matchMedia("(prefers-color-scheme: dark)").matches
-			)
-				dispatch(ROOT_MODULE, UPDATE_THEME, {
-					theme: DARK_THEME,
-				});
-			return;
-		} else
-			dispatch(ROOT_MODULE, UPDATE_THEME, {
-				theme: this.$storage.get(THEME_CACHE_KEY).theme,
-			});
-	},
-	created() {
-		this.$http
-			.get("/")
-			.then((res: AxiosResponse) => {
-				console.log(res);
-			})
-			.catch((err: AxiosError) => {
-				console.log(err);
-			});
-	},
-})
+@Component
 export default class App extends Vue {
 	private get layout(): string | null {
 		const value =
