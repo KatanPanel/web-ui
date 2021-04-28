af5d1222d0abdfbcb9e2aee3c28b06b4fcaf93a2
Natan Vieira <itroken99@gmail.com>
2021-04-28 18:36:45 -0300

    fix(ui/dropdown): fix dropup detection

--- src/app/shared/components/ui/dropdown/VDropdown.vue
+++ src/app/shared/components/ui/dropdown/VDropdown.vue
@@ -1,34 +1,72 @@
 <template>
 	<div
-		@click.stop="toggle"
-		tabindex="0"
-		v-click-outside="close"
 		:active="isOpen"
 		class="v--dropdown"
+		v-click-outside="close"
+		tabindex="0"
 		@keyup.enter.stop="toggle"
+		@click.stop="toggle"
 	>
-		<slot />
-		<div class="v--dropdown-item-list-mask">
-			<div v-show="isOpen" class="v--dropdown-item-list">
-				<slot name="items" />
+		<v-flex-box>
+			<slot />
+			<div v-if="!isOpen" key="closed" class="v--dropdown-caret-icon">
+				<v-icon name="caret-down" />
 			</div>
-		</div>
+			<div v-else key="open" class="v--dropdown-caret-icon">
+				<v-icon name="caret-up" />
+			</div>
+		</v-flex-box>
+		<ul
+			v-cloak
+			ref="list"
+			:class="[`drop${direction}`]"
+			:style="{ visibility: isOpen ? 'visible' : 'hidden' }"
+			class="v--dropdown-item-list"
+		>
+			<slot name="items" />
+		</ul>
 	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Ref, Vue } from "vue-property-decorator";
+import VIcon from "@/app/shared/components/ui/icon/VIcon.vue";
+import VFlexBox from "@/app/shared/components/ui/layout/VFlexBox.vue";
 
-@Component
+@Component<VDropdown>({
+	components: { VFlexBox, VIcon },
+	beforeMount(): void {
+		window.addEventListener("resize", this.calculateDirection);
+		window.addEventListener("scroll", this.calculateDirection);
+	},
+	mounted(): void {
+		this.calculateDirection();
+	},
+	beforeDestroy(): void {
+		window.removeEventListener("resize", this.calculateDirection);
+		window.removeEventListener("scroll", this.calculateDirection);
+	}
+})
 export default class VDropdown extends Vue {
-	private isOpen = false;
+	isOpen = false;
+	direction = "down";
 
-	public toggle(): void {
+	@Ref("list")
+	readonly list!: HTMLElement;
+
+	calculateDirection(): void {
+		const windowHeight = window.innerHeight;
+		const listHeight = this.list.offsetHeight;
+		const position = this.$el.getBoundingClientRect().y;
+		this.direction = position + listHeight >= windowHeight ? "up" : "down";
+	}
+
+	toggle(): void {
 		this.isOpen = !this.isOpen;
 		this.$emit("toggle", this.isOpen);
 	}
 
-	public close(): void {
+	close(): void {
 		if (!this.isOpen) return;
 
 		this.isOpen = false;
