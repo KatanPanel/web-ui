2a421a4ca4840526c842bb599635e06fc6d1fc47
Natan Vieira <natanvnascimento@gmail.com>
2021-09-08 23:41:56 -0300

    feat: create `app-help` module

---
+++ src/app/app-help/views/AppHelpIndex.vue
@@ -0,0 +1,46 @@
+<!--
+  - Copyright (c) 2020-present Katan
+  -
+  - Permission is hereby granted, free of charge, to any person obtaining a copy
+  - of this software and associated documentation files (the "Software"), to deal
+  - in the Software without restriction, including without limitation the rights
+  - to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  - copies of the Software, and to permit persons to whom the Software is
+  - furnished to do so, subject to the following conditions:
+  -
+  - The above copyright notice and this permission notice shall be included in all
+  - copies or substantial portions of the Software.
+  -
+  - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  - IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  - FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  - AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  - LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  - OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  - SOFTWARE.
+  -->
+
+<template>
+	<v-container>
+		<h4><b v-text="$t('views.help.title')" /></h4>
+	</v-container>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
+import { AppNavigationWindowChildMixin } from "@/app/app-navigation/mixins/app-navigation-window-child-mixin.component";
+import { MetaInfo } from "vue-meta";
+import { generateMetaInfo } from "@/app/shared/utils/builtin";
+import VContainer from "@/app/shared/components/ui/layout/VContainer.vue";
+
+@Component<AppHelpIndex>({
+	components: { VContainer },
+	metaInfo(): MetaInfo {
+		return generateMetaInfo(this.$i18n, "help");
+	}
+})
+export default class AppHelpIndex extends mixins(
+	AppNavigationWindowChildMixin
+) {}
+</script>
