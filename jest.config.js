f1b7a1d0d2fbd8662776eb49c1141cefb3230aa6
Natan Vieira <natanvnascimento@gmail.com>
2021-09-12 16:58:22 -0300

    chore: add `jest` dependency

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
 	preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+	moduleFileExtensions: ["js", "json", "vue"],
+	transform: {
+		".*\\.(vue)$": "vue-jest"
+	}
 };
