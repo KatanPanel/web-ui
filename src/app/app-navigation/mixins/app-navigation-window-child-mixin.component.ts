7e0618109a12565509df165520bde5237f65a052
Natan Vieira <natanvnascimento@gmail.com>
2021-09-12 15:19:27 -0300

    docs(app-navigation): add docs to `AppNavigationWindowChildMixin`

--- src/app/app-navigation/mixins/app-navigation-window-child-mixin.component.ts
+++ src/app/app-navigation/mixins/app-navigation-window-child-mixin.component.ts
@@ -27,8 +27,24 @@ import type {
 } from "@/app/app-navigation/models/app-navigation-window.model";
 import { isUndefined } from "@/app/shared/utils";
 
+/**
+ * Attribute added to Vue instances created from navigation windows.
+ */
+export const NAVIGATION_WINDOW_ATTR = Symbol();
+
+/**
+ * This mixin is responsible for doing post-build and pre-destruct handling of
+ * components created from {@link AppNavigationWindow}.
+ *
+ * When a component is created from a navigation window, an attribute is applied to it,
+ * this is especially necessary because this mixin is applied globally,
+ * so it will be applied to any instance created by Vue.
+ *
+ * The attribute serves as a form of instance filtering to avoid unnecessary computations.
+ */
 @Component<AppNavigationWindowChildMixin>({
 	created(): void {
+		console.log("Navigation window created", this);
 		if (isUndefined(this.windowChild?.title))
 			this.updateWindow({
 				title: this.$i18n.t(
@@ -40,7 +56,17 @@ import { isUndefined } from "@/app/shared/utils";
 		if (isUndefined(this.window)) this.$destroy();
 	},
 	mounted(): void {
-		if ((this as any)._isDestroyed) this.$el.remove();
+		/**
+		 * Vue applies an "_isDestroyed" property to components
+		 * that have already been destroyed but not discarded.
+		 */
+		const destroyable = (this as unknown) as { _isDestroyed: boolean };
+
+		/**
+		 * Vue 2 does not allow complete destruction of the component,
+		 * so we have to remove the DOM element manually.
+		 */
+		if (destroyable._isDestroyed) this.$el.remove();
 	}
 })
 export class AppNavigationWindowChildMixin extends Vue {
@@ -52,5 +78,25 @@ export class AppNavigationWindowChildMixin extends Vue {
 
 	protected updateWindow(value: Partial<AppNavigationWindowChildren>): void {
 		this.window?.update(this.windowChild, value);
+	}
+
+	/**
+	 * Returns a unique identification line from the input value prefixed with
+	 * the identification number of the current navigation window.
+	 *
+	 * This does not guarantee that your id is especially unique,
+	 * the concept "unique" is related to referencing the input value in the DOM.
+	 *
+	 * When there is more than one browser window available in the DOM,
+	 * their IDs can conflict, after all, they are in the same DOM and both use the id,
+	 * so use this so there is no conflict.
+	 *
+	 * It is preferable that you reference elements in the DOM using {@link Vue.$el}.
+	 *
+	 * @param input the input value
+	 * @protected
+	 */
+	public uniqueId(input: string): string {
+		return this.window?.id + "_" + input;
 	}
 }
