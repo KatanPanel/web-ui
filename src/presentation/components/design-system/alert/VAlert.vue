192a9a673c8b031e5a1824ab605057417edc8dc3
Natan Vieira <itroken99@gmail.com>
2022-08-11 23:23:57 -0300

    Fix alert component

--- src/presentation/components/design-system/alert/VAlert.vue
+++ src/presentation/components/design-system/alert/VAlert.vue
@@ -1,13 +1,15 @@
 <template>
-	<div :class="[$style.root, `alert--variant-${variant}`]">
+	<div class="alert" :class="`alert--variant-${variant}`">
 		<div class="alert__icon">
 			<Component :is="getComponent" />
 		</div>
-		<div class="alert__title">
-			<slot>Title</slot>
-		</div>
-		<div class="alert__description" v-if="$slots.description">
-			<slot name="description"> Description</slot>
+		<div class="alert__header">
+			<div class="alert__title">
+				<slot>Title</slot>
+			</div>
+			<div class="alert__description" v-if="$slots.description">
+				<slot name="description"> Description</slot>
+			</div>
 		</div>
 	</div>
 </template>
@@ -28,6 +30,7 @@ export default class VAlert extends Vue {
 		if (this.variant == "error") path = "AlertCircleOutline";
 
 		if (!isUndefined(path))
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
 			return require(/* webpackChunkName: "icon-[request]" */
 			"vue-material-design-icons/" + path + ".vue").default;
 
@@ -35,34 +38,40 @@ export default class VAlert extends Vue {
 	}
 }
 </script>
-<style lang="scss" module>
-.root {
-	background-color: var(--kt-background-surface-high);
-	box-shadow: inset 0 0 0 1.5px var(--kt-border-low);
-	border-radius: 8px;
-}
-</style>
 <style lang="scss" scoped>
 $base-padding: 1.2rem;
 
+.alert {
+	background-color: var(--kt-background-surface-high);
+	box-shadow: inset 0 0 0 1.5px var(--kt-border-low);
+	border-radius: 8px;
+	display: flex;
+}
+
 .alert__title {
-	padding: $base-padding;
 	font-size: 14px;
 	font-weight: 500;
+	padding-top: 1.2rem;
+	padding-right: $base-padding;
+}
+
+.alert__icon {
+	padding: $base-padding;
 }
 
 .alert__description {
-	font-size: 12px;
+	font-size: 13px;
 	margin-top: 4px;
-	padding: $base-padding;
-	padding-top: 0;
+	padding-bottom: $base-padding;
+	padding-right: $base-padding;
 }
 
 .alert--variant-error {
 	background-color: var(--kt-content-negative-overlay);
 	box-shadow: inset 0 0 0 1.5px var(--kt-content-negative-overlay);
 
-	.alert__title {
+	.alert__title,
+	.alert__icon {
 		color: var(--kt-content-negative);
 	}
 }
