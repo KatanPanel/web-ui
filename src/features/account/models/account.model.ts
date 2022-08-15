885e4a4f99b1f0dffa900c5d058958cff42ba4ed
Natan Vieira <itroken99@gmail.com>
2022-08-15 19:50:31 -0300

    Add "email" and "avatar" fields to Account model

--- src/features/account/models/account.model.ts
+++ src/features/account/models/account.model.ts
@@ -1,7 +1,9 @@
 export interface Account {
 	readonly id: string;
 	readonly username: string;
+	readonly email: string;
 	readonly createdAt: Date;
 	readonly updatedAt: Date;
 	readonly lastLoggedInAt?: Date;
+	readonly avatar?: string;
 }
