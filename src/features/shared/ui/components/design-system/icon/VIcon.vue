c843e435bc88e581bea4b6fc4aff7c82a4b27ef7
Natan Vieira <itroken99@gmail.com>
2022-08-15 02:25:46 -0300

    Add fallback if icon is not found

--- src/features/shared/ui/components/design-system/icon/VIcon.vue
+++ src/features/shared/ui/components/design-system/icon/VIcon.vue
@@ -1,18 +1,33 @@
 <template>
-	<Component :is="getComponent" />
+	<Component :is="component" />
 </template>
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-facing-decorator";
+import logService from "@/features/shared/data/log.service";
+import { ConsolaLogObject } from "consola";
 
 @Component
 export default class VIcon extends Vue {
 	@Prop({ type: String, required: true })
 	private readonly name!: string;
 
-	get getComponent() {
+	get component(): unknown {
+		try {
+			return this.requireIcon(this.name);
+		} catch (e: unknown) {
+			logService.error({
+				message: "Icon not found",
+				additional: [e],
+				args: [this.name]
+			} as ConsolaLogObject);
+			return this.requireIcon("Help" /* fallback */);
+		}
+	}
+
+	requireIcon(name: string): unknown {
 		// eslint-disable-next-line @typescript-eslint/no-var-requires
 		return require(/* webpackChunkName: "icon-[request]" */
-		"vue-material-design-icons/" + this.name + ".vue").default;
+		"vue-material-design-icons/" + name + ".vue").default;
 	}
 }
 </script>
