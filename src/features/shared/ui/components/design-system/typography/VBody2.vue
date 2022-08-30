16cdb4a312d39329ca10e93afdac99ca4d8fc2d9
Natan Vieira <itroken99@gmail.com>
2022-08-29 23:40:38 -0300

    Reduce VBody2 font size and change color and margin

--- src/features/shared/ui/components/design-system/typography/VBody2.vue
+++ src/features/shared/ui/components/design-system/typography/VBody2.vue
@@ -14,7 +14,9 @@ export default class VBody2 extends Vue {}
 .root {
 	font-family: var(--kt-body-font);
 	font-weight: 400;
-	font-size: 15px;
+	font-size: 14px;
 	letter-spacing: 0;
+	color: var(--kt-content-neutral);
+	margin: 1.2rem 0 2.4rem;
 }
 </style>
