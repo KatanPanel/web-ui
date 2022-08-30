43b28c6b7c3263bb8831ee85dd19f24fbb36dde2
Natan Vieira <itroken99@gmail.com>
2022-08-29 23:38:52 -0300

    Create VSection component

---
+++ src/features/shared/ui/components/design-system/grid/VSection.vue
@@ -0,0 +1,18 @@
+<template>
+	<section :class="$style.root">
+		<slot />
+	</section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-facing-decorator";
+
+@Component
+export default class VSection extends Vue {}
+</script>
+
+<style lang="scss" module>
+.root {
+	margin-top: 2.4rem;
+}
+</style>
