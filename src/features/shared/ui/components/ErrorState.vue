1504cf2fe066bb495b5f9931542039c86e24da6a
Natan Vieira <itroken99@gmail.com>
2022-08-29 23:37:49 -0300

    Create LoadingState and ErrorState

---
+++ src/features/shared/ui/components/ErrorState.vue
@@ -0,0 +1,14 @@
+<template>
+	<h4>Error state ;)</h4>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-facing-decorator";
+
+@Component({
+	emits: ["retry"]
+})
+export default class ErrorState extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/features/shared/ui/components/LoadingState.vue
@@ -0,0 +1,10 @@
+<template>
+	<h4>Loading...</h4>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-facing-decorator";
+
+@Component
+export default class LoadingState extends Vue {}
+</script>
