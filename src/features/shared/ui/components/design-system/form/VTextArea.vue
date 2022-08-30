6eaf557eb2fcae9bd331f8a215b3242e36202d04
Natan Vieira <itroken99@gmail.com>
2022-08-29 23:43:53 -0300

    Create VTextArea component

---
+++ src/features/shared/ui/components/design-system/form/VTextArea.vue
@@ -0,0 +1,22 @@
+<template>
+	<textarea
+		:class="$style.root"
+		@input="$emit('update:modelValue', $event.target.value)"
+	/>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-facing-decorator";
+
+@Component
+export default class VTextArea extends Vue {}
+</script>
+
+<style lang="scss" module>
+.root {
+	background-color: var(--kt-background-surface);
+	padding: 0.8rem;
+	width: 100%;
+	height: 100%;
+}
+</style>
