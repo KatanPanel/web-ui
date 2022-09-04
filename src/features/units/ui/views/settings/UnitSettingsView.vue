4359a8c3050da30630ac9c46e3a7421f5dc25edc
Natan Vieira <itroken99@gmail.com>
2022-09-04 19:56:55 -0300

    Use single-page unit settings view

--- src/features/units/ui/views/settings/UnitSettingsView.vue
+++ src/features/units/ui/views/settings/UnitSettingsView.vue
@@ -3,35 +3,37 @@
 		<PageHeader>
 			<template #title><span v-t="'units.settings.title'" /></template>
 		</PageHeader>
-		<VTabList accessibility-label="Settings Tab">
-			<VTab id="general" label="General">
-				<UnitSettingsGeneralTab />
-			</VTab>
-			<VTab id="advanced" label="Advanced">
-				<UnitSettingsAdvancedTab />
-			</VTab>
-		</VTabList>
+		<VSection>
+			<VForm>
+				<VFieldSet>
+					<VLabel>Unit name</VLabel>
+					<VInput :value="unit.name" readonly />
+				</VFieldSet>
+			</VForm>
+		</VSection>
 	</VContainer>
 </template>
 
 <script lang="ts">
 import { Component, Inject, Vue } from "vue-facing-decorator";
 import { Unit } from "@/features/units/models/unit.model";
-import VTabList from "@/features/shared/ui/components/design-system/tabs/VTabList.vue";
-import VTab from "@/features/shared/ui/components/design-system/tabs/VTab.vue";
-import UnitSettingsGeneralTab from "@/features/units/ui/components/settings/UnitSettingsGeneralTab.vue";
-import UnitSettingsAdvancedTab from "@/features/units/ui/components/settings/UnitSettingsAdvancedTab.vue";
 import PageHeader from "@/features/shared/ui/components/PageHeader.vue";
+import VInput from "@/features/shared/ui/components/design-system/form/VInput.vue";
+import VFieldSet from "@/features/shared/ui/components/design-system/form/VFieldSet.vue";
+import VLabel from "@/features/shared/ui/components/design-system/form/VLabel.vue";
 import VContainer from "@/features/shared/ui/components/design-system/grid/VContainer.vue";
+import VForm from "@/features/shared/ui/components/design-system/form/VForm.vue";
+import VSection from "@/features/shared/ui/components/design-system/grid/VSection.vue";
 
 @Component({
 	components: {
 		PageHeader,
-		UnitSettingsAdvancedTab,
-		UnitSettingsGeneralTab,
-		VTab,
-		VTabList,
-		VContainer
+		VContainer,
+		VForm,
+		VFieldSet,
+		VInput,
+		VLabel,
+		VSection
 	}
 })
 export default class UnitSettingsView extends Vue {
