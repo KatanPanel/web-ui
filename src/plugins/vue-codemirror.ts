88dad8f45c1326bffec50341975e756d720ec336
Natan Vieira <itroken99@gmail.com>
2022-09-24 23:27:09 -0300

    Customize code mirror editor

--- src/plugins/vue-codemirror.ts
+++ src/plugins/vue-codemirror.ts
@@ -1,13 +1,28 @@
-import { basicSetup } from "codemirror";
-import VueCodeMirror from "vue-codemirror";
 import { App } from "vue";
+import VueCodeMirror from "vue-codemirror";
+import { basicSetup } from "codemirror";
+import { json, jsonParseLinter } from "@codemirror/lang-json";
+import { xml } from "@codemirror/lang-xml";
+import { markdown } from "@codemirror/lang-markdown";
+import { linter, lintGutter } from "@codemirror/lint";
+import { hyperLink } from "@uiw/codemirror-extensions-hyper-link";
+import { color } from "@uiw/codemirror-extensions-color";
 
 const install = (app: App) => {
 	app.use(VueCodeMirror, {
 		autoFocus: true,
 		indentWithTab: true,
 		tabSize: 4,
-		extensions: [basicSetup]
+		extensions: [
+			basicSetup,
+			json(),
+			xml(),
+			markdown(),
+			linter(jsonParseLinter()),
+			lintGutter(),
+			hyperLink,
+			color
+		]
 	})
 }
 
