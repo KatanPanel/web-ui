bacdddee4d124f9c48042252d72861f51b754558
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-24 16:47:47 -0300

    Remove comment from main file

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import "@/assets/styles/main.scss";
 import App from "@/createApp";
 
 const app = App;
-
-// assigned all declared augmented from types/shims-vue.d.ts
-Object.assign(app.config.globalProperties, { $isDevelopmentMode: process.env.NODE_ENV !== "production" });
+Object.assign(app.config.globalProperties, {
+	$isDevelopmentMode: process.env.NODE_ENV !== "production"
+});
 
 app.mount("#app");
