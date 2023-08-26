f8210b771e195ec7001d2ad4b0729d2c737ee2c4
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-26 19:36:27 -0300

    Remove PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { resolve, dirname } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
@@ -12,8 +11,7 @@ export default defineConfig({
         vueJsx(),
         VueI18nPlugin({
             include: resolve(dirname(fileURLToPath(import.meta.url)), './src/lang/**'),
-        }),
-        VitePWA()
+        })
     ],
     resolve: {
         alias: {
