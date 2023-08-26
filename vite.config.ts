8965a7b84467817477474a40f08eb5925fe8d0f7
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-26 20:57:52 -0300

    Apply splitVendorChunk plugin to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import { resolve, dirname } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import pkg from "./package.json"
+import { splitVendorChunkPlugin } from 'vite'
+
+process.env.VITE_APP_VERSION = pkg.version
 
 export default defineConfig({
     plugins: [
+        splitVendorChunkPlugin(),
         vue(),
         vueJsx(),
         VueI18nPlugin({
