7537d5b5b96cf5645f84452c49715f9f0ec46484
Natan Vieira do Nascimento <itroken99@gmail.com>
2023-08-30 22:12:58 -0300

    Fetch backend info on app load

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
-  <router-view />
+  <LoadingState
+    v-if="isLoading"
+    :class="$style.loading"
+  />
+  <router-view v-else />
   <ModalsContainer />
 </template>
 
 <script setup lang="ts">
 import { ModalsContainer } from "vue-final-modal"
+import LoadingState from "@/modules/platform/ui/components/LoadingState.vue"
+import { ref } from "vue"
+import serverService from "@/modules/platform/api/services/server.service"
+
+const isLoading = ref(true)
+
+serverService.getInfo().finally(() => (isLoading.value = false))
 </script>
 <style lang="scss">
 #app {
-	display: flex;
-	display: -ms-flexbox;
-	flex-direction: column;
-	height: 100%;
+    display: flex;
+    display: -ms-flexbox;
+    flex-direction: column;
+    height: 100%;
+}
+</style>
+<style lang="scss" module>
+.loading {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
 }
 </style>
